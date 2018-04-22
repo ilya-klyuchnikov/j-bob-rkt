2a2668d692a2de589778d4c1426a2d9744fbcc16
Ilya Klyuchnikov <ilyaklyuchnikov@fb.com>
2018-04-22 22:50:59 +0100

    minimal api

--- j-bob-api.rkt
+++ j-bob-api.rkt
@@ -1,5 +1,21 @@
 #lang racket/base
 
 (module j-bob-api (submod "j-bob.rkt" j-bob)
-  (provide
-   (all-from-out (submod "j-bob.rkt" j-bob))))
+  (provide   
+   ; to be able to apply functions
+   #%app
+   ; to be able to create symbols (quoting)
+   quote
+   ; to be able to create modules
+   #%module-begin
+   provide
+   all-defined-out
+   all-from-out
+   ; API
+   defun
+   dethm
+   J-Bob/step
+   J-Bob/define
+   J-Bob/prove
+   prelude      
+   list-extend))
